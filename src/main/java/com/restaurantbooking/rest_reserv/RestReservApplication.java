21391d598e98ee62891686d1ea3bd29876486096
YaroDido <jarik.dido@gmail.com>
2023-08-06 17:36:09 +0200

    The first setup for ptoject.

---
+++ src/main/java/com/restaurantbooking/rest_reserv/RestReservApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantbooking.rest_reserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestReservApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restaurantbooking/rest_reserv/RestReservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantbooking.rest_reserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestReservApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
