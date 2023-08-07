61c79f27c495ff3a21d186c2081489dc4374ce98
YaroDido <jarik.dido@gmail.com>
2023-08-08 00:37:13 +0200

    frontend for application

---
+++ src/main/java/com/restaurantbooking/rest_reserv/controller/FrontendController.java
@@ -0,0 +1,69 @@
+package com.restaurantbooking.rest_reserv.controller;
+
+import com.restaurantbooking.rest_reserv.entity.Reservation;
+import com.restaurantbooking.rest_reserv.entity.UserProfile;
+import com.restaurantbooking.rest_reserv.service.ReservationService;
+import com.restaurantbooking.rest_reserv.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class FrontendController {
+    private final UserProfileService userProfileService;
+    private final ReservationService reservationService;
+
+    @Autowired
+    public FrontendController(ReservationService reservationService, UserProfileService userProfileService) {
+        this.reservationService = reservationService;
+        this.userProfileService = userProfileService;
+    }
+
+    @GetMapping("/register-and-reserve")
+    public String registrationAndReservationForm(Model model) {
+        model.addAttribute("userProfile", new UserProfile()); // Initialize an empty user profile object
+        return "registrationAndReservation";
+    }
+
+    @PostMapping("/register-and-reserve")
+    public String registerAndReserve(
+            @ModelAttribute("userProfile") UserProfile userProfile,
+            @RequestParam("reservationDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime reservationDateTime,
+            @RequestParam("tableNumber") int tableNumber,
+            @RequestParam("quantityOfCustomers") int quantityOfCustomers) {
+
+        // Register the user profile
+        UserProfile savedUserProfile = userProfileService.createUserProfile(userProfile);
+
+        // Create a reservation
+        Reservation reservation = new Reservation();
+        reservation.setCustomerName(savedUserProfile.getUsername());
+        reservation.setReservationDateTime(reservationDateTime);
+        reservation.setTableNumber(tableNumber);
+        reservation.setQuantityOfCustomers(quantityOfCustomers);
+        reservationService.createReservation(reservation);
+
+        return "successPage"; // A page indicating successful registration and reservation
+    }
+}
+
+//    @GetMapping("/reservation")
+//    public String reservationFrom(Model model) {
+//        List<UserProfile> userProfiles = userProfileService.getAllUserProfiles();
+//        model.addAttribute("userProfiles", userProfiles);
+//        return "reservation";
+//    }
+//
+//    @GetMapping("/create-user-profile")
+//    public String createUserProfileFrom(Model model) {
+//        return "createUserProfile";
+//    }
+
